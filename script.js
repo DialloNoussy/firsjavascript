3542c120b9320aef74a8acc20e87507f0e225d8b
DialloNoussy <abdourahmane.noussydiallo@gmail.com>
2022-07-12 19:45:38 +0200

    debut des function

---
+++ script.js
@@ -0,0 +1,14 @@
+let firstname= "amadou";
+
+function isconnected() {
+    console.log(firstname);
+}
+
+function isnotconnected() {
+    let firstname= "parfait";
+    console.log(firstname);
+    
+}
+fu
+isconnected();
+isnotconnected();
